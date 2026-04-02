7845faefa0921e017fc6bf1e78d7e8d86f03e783
Vy Ngo Chi <ncv.ngovy@gmail.com>
2026-04-02 20:21:04 +0700

    feat: implement user authentication with registration, login, and logout functionalities; add email verification and Swagger documentation

--- src/app.ts
+++ src/app.ts
@@ -26,7 +26,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 //Routes
 app.use("/api", routes);
 
-app.get("health", (_req, _res) => _res.json({ status: "ok" }));
+app.get("/health", (_req, _res) => _res.json({ status: "ok" }));
 
 app.use(errorHandler);
 
