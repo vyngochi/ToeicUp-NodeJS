2b31a6e6a17a924141bb31e9813ecd857bd7e7a5
Vy Ngo Chi <ncv.ngovy@gmail.com>
2026-04-03 22:38:59 +0700

    fix(mailer): add family option to nodemailer transport configuration

--- src/config/mailer.ts
+++ src/config/mailer.ts
@@ -5,6 +5,7 @@ export const transporter = nodemailer.createTransport({
   host: config.mail.host,
   port: config.mail.port,
   secure: false,
+  family: 4,
   auth: {
     user: config.mail.user,
     pass: config.mail.pass,
