f89a54473e0e952fe27f1a0c4fd42006366c349e
Vy Ngo Chi <ncv.ngovy@gmail.com>
2026-04-04 14:00:36 +0700

    fix(mailer): enable secure connection and ensure timeout settings are applied

--- src/config/mailer.ts
+++ src/config/mailer.ts
@@ -4,10 +4,12 @@ import { config } from ".";
 export const transporter = nodemailer.createTransport({
   host: config.mail.host,
   port: config.mail.port,
-  secure: false,
+  secure: true,
   family: 4,
   auth: {
     user: config.mail.user,
     pass: config.mail.pass,
   },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
 } as nodemailer.TransportOptions);
