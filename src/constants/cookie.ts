d281a5dc2bd8c0008d9ac1905549ebc7423c8e0a
Vy Ngo Chi <ncv.ngovy@gmail.com>
2026-04-05 18:22:40 +0700

    feat(cookie): enhance COOKIE_OPTIONS with type definition and additional properties

--- src/constants/cookie.ts
+++ src/constants/cookie.ts
@@ -1,8 +1,12 @@
+import { CookieOptions } from "express";
+
 export const REFRESH_COOKIE = "refreshToken";
 
-export const COOKIE_OPTIONS = {
+export const COOKIE_OPTIONS: CookieOptions = {
   httpOnly: true,
   secure: true,
-  sameSite: "none" as const,
+  sameSite: "none",
   maxAge: 7 * 24 * 60 * 60 * 1000,
+  path: "/",
+  partitioned: true,
 };
